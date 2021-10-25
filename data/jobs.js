29731dd46bb67e30b222162c39bd8c93367651d9
Alexandre Insua <alexandreinsua@gmail.com>
2021-10-25 21:01:07 +0200

    feat(dynamic-data) jobs file added

---
+++ data/jobs.js
@@ -0,0 +1,44 @@
+export const jobs = [
+    {
+        "position": "Junior Software Engineer",
+        "company": "Leads Corporation Ltd",
+        "iniDate": "2020-03",
+        "endDate": "2020-06",
+        "description": "I worked on Banking Solution. I worked on AngularJS, Web Api, Dot Net and C#, Oracle Database"
+    },
+    {
+        "position": "Junior Software Engineer",
+        "company": "Syntec Consulting Ltd",
+        "iniDate": "2018-07",
+        "endDate": "present",
+        "description": "Provides various front-end and back-end related services. I worked on AngualarJS, Web Api, Javascript, Dot Net and C#"
+    },
+    {
+        "position": "Brand Promoter",
+        "company": "Ipay System Ltd",
+        "iniDate": "2017-01",
+        "endDate": null,
+        "description": "Promote Mobile Banking Apps "
+    },
+    {
+        "position": "Organiser and Host",
+        "company": "Microsoft Technical Community Bangladesh",
+        "iniDate": "2017-09",
+        "endDate": "present",
+        "description": "Event Organise and host for Microsoft event online and offline both"
+    },
+    {
+        "position": "Microsoft Student Partner",
+        "company": "Microsoft Bangladesh",
+        "iniDate": "2017",
+        "endDate": "2018",
+        "description": "Microsoft Student Partner from bangladesh."
+    },
+    {
+        "position": "Volunteer",
+        "company": "NASA Space APPS Challenge at Bangladesh",
+        "iniDate": "2014",
+        "endDate": "2017",
+        "description": "I arranged NASA Space APPS Challenge Bootcamp in my University for 3 years. I also do volunteering for 3 years in NASA Space APPS Challenge. It was great Experience. "
+    }
+]
